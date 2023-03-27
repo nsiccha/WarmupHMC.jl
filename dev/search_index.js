c0f6e683da26ddc138cd857494a6bc6448c236a6
Documenter.jl <documenter@juliadocs.github.io>
2023-03-27 14:08:12 +0000

    build based on fc0f1a5

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#WarmupHMC.jl","page":"WarmupHMC.jl","title":"WarmupHMC.jl","text":"","category":"section"},{"location":"","page":"WarmupHMC.jl","title":"WarmupHMC.jl","text":"Documentation for WarmupHMC.jl","category":"page"}]
+[{"location":"#WarmupHMC.jl","page":"WarmupHMC.jl","title":"WarmupHMC.jl","text":"","category":"section"},{"location":"","page":"WarmupHMC.jl","title":"WarmupHMC.jl","text":"Documentation for WarmupHMC.jl.","category":"page"}]
 }

7e44a579df2946f637b4bf7bfa9e582f9f206a65
Documenter.jl <documenter@juliadocs.github.io>
2023-03-30 18:52:40 +0000

    build based on ebb1175

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#WarmupHMC.jl","page":"WarmupHMC.jl","title":"WarmupHMC.jl","text":"","category":"section"},{"location":"","page":"WarmupHMC.jl","title":"WarmupHMC.jl","text":"Documentation for WarmupHMC.jl.","category":"page"}]
+}

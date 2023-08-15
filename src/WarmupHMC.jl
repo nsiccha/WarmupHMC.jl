47b493e0d35babde5a09ab563ccc9a4becc5dbec
Nikolas Siccha <nikolassiccha@gmail.com>
2023-08-15 12:29:44 +0300

    autocommit

--- src/WarmupHMC.jl
+++ src/WarmupHMC.jl
@@ -1,9 +1,10 @@
 module WarmupHMC
 
-export regularize, to_x1, to_xc, klp, klps#, klps_plot!
+export regularize, to_x1, to_xc, klp, klps, approximately_whitened#, klps_plot!
 
 using DynamicObjects
-using Distributions, LinearAlgebra
+using Random, Distributions, LinearAlgebra
+using LogDensityProblems
 
 regularize(sample_covariance, no_draws, regularization_no_draws=5, regularization_constant=1e-3) = (
   no_draws / ((no_draws + regularization_no_draws) * (no_draws - 1)) * sample_covariance + regularization_constant * (regularization_no_draws / (no_draws + regularization_no_draws)) * I
@@ -42,4 +43,97 @@ klps(x1s, means, log_sds, cs) = [klp(x1s, means, log_sds, c) for c in cs]
 # reparametrize(what::DiagonalScaling, parameters) = what.scaling .* parameters
 # unreparametrize(what::DiagonalScaling, reparameters) = what.scaling .\ reparameters
 # logjacobian(what::DiagonalScaling, ::Any, ::Any) = 0
+
+struct HouseholderReflector{T} <: AbstractMatrix{T}
+    v::Vector{T}
+end
+HouseholderReflector(source::AbstractVector, target::AbstractVector) = HouseholderReflector(normalize(source - target))
+HouseholderReflector(source::AbstractVector, target::Integer) = begin 
+    e = zeros(length(source))
+    e[target] = 1
+    HouseholderReflector(source, e)
+end
+Base.:*(lhs::HouseholderReflector, rhs::AbstractVector) = rhs - 2 * lhs.v * (lhs.v' * rhs)
+Base.:*(lhs::HouseholderReflector, rhs::AbstractMatrix) = rhs - 2 * lhs.v * (lhs.v' * rhs)
+Base.:*(lhs::HouseholderReflector, rhs::Diagonal) = rhs - 2 * lhs.v * (lhs.v' * rhs)
+Base.transpose(lhs::HouseholderReflector) = lhs
+Base.adjoint(lhs::HouseholderReflector) = lhs
+Base.size(lhs::HouseholderReflector) = (length(lhs.v), length(lhs.v))
+
+do_nothing(args...; kwargs...) = nothing
+projected_out(x, n::AbstractVector) = x - n * (n' * x)
+
+function approximate_whitening(
+    logdensity; 
+    dt0=1e-6, rng=Xoshiro(0), n_parameters = LogDensityProblems.dimension(logdensity), n_iterations=n_parameters, x=randn(rng, n_parameters),
+    dt_speedup=8, dt_mul=1. / dt_speedup, pack=do_nothing,
+    twosided=false
+)
+    lpdfg(x) = LogDensityProblems.logdensity_and_gradient(logdensity, x)[2]
+    
+    dt = collect(Diagonal(fill(dt_mul, n_parameters)))
+
+    a = lpdfg(x)
+    # pack(x, a)
+    for iteration in 1:n_iterations
+        dt[iteration:end, :] *= dt0
+        v = randn(rng, n_parameters)
+        # dx = dt' * (v + .5 * (dt*a))
+        dxv = dt' * v
+        dxa = dt' * (dt * .5a)
+        xr = x + dxv + dxa
+        ar = lpdfg(xr)
+
+        dx, da, v = if twosided == true || twosided == :first && iteration == 1
+            xl = x - dxv + dxa
+            al = lpdfg(xl)
+            xr-xl,ar-al,ar-2a+al
+        else
+            xr-x,ar-a,ar-a
+        end
+        x = xr
+        a = ar
+        # pack(x, a)
+        dx = dt * dx
+        da = dt * da
+        v = dt * v
+        v[1:iteration-1] .= 0
+        nv = v |> normalize
+        scale = sqrt(abs(dot(nv, dx) / dot(nv, da)))
+        if nv[iteration] != 1
+            hr = HouseholderReflector(nv, iteration)
+            dt = hr * dt
+        end
+        dt[iteration:end, :] ./= dt0
+        dt[iteration, :] .*= scale
+        dt0 = dt_speedup*scale
+    end
+    dt ./= dt_mul
+    return dt
+end
+
+struct ScaledLogDensity{L, T} 
+    logdensity::L
+    transform::T
+end
+ScaledLogDensity(what::ScaledLogDensity, transform) = ScaledLogDensity(
+    what.logdensity, what.transform * transform
+)
+LogDensityProblems.dimension(what::ScaledLogDensity) = LogDensityProblems.dimension(
+    what.logdensity
+)
+LogDensityProblems.capabilities(::Type{<:ScaledLogDensity{L,T}}) where {L,T} = LogDensityProblems.capabilities(L)
+transform(what::ScaledLogDensity, xi) = what.transform * xi
+LogDensityProblems.logdensity(what::ScaledLogDensity, xi) = LogDensityProblems.logdensity(what.logdensity, transform(what, xi))
+LogDensityProblems.logdensity_and_gradient(what::ScaledLogDensity, xi) = begin 
+    l, g = LogDensityProblems.logdensity_and_gradient(what.logdensity, transform(what, xi))
+    l, what.transform' * g
+end
+
+approximately_whitened(logdensity; kwargs...) = ScaledLogDensity(
+    logdensity, approximate_whitening(logdensity; kwargs...)'
+)
+
+
+
 end

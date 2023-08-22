014751e37146174013f5f1514b86d6c5d6326141
Nikolas Siccha <nikolas.siccha@aalto.fi>
2023-08-22 12:23:17 +0300

    autocommit

--- ext/WarmupHMCOptimReverseDiffExt.jl
+++ ext/WarmupHMCOptimReverseDiffExt.jl
@@ -3,10 +3,12 @@ module WarmupHMCOptimReverseDiffExt
 using WarmupHMC, Optim, ReverseDiff
 
 WarmupHMC.find_reparametrization(::Val{:ReverseDiff}, source, draws::AbstractMatrix; iterations=5, method=LBFGS(), verbose=false) = begin 
+    init_arg = WarmupHMC.reparametrization_parameters(source)
     loss = WarmupHMC.reparametrization_loss_function(source, draws)
+    loss_tape = ReverseDiff.compile(ReverseDiff.GradientTape(loss, init_arg))
     loss_g!(g, arg) = ReverseDiff.gradient!(g, loss_tape, arg)
     optimization_result = optimize(
-        loss, loss_g!, WarmupHMC.reparametrization_parameters(source), method, 
+        loss, loss_g!, init_arg, method, 
         Optim.Options(iterations=iterations)
     )
     verbose && display(optimization_result)

5e026964160d69c8b04467ad06153bd6a304a565
Nikolas Siccha <nikolas.siccha@aalto.fi>
2023-08-22 12:04:04 +0300

    autocommit

--- ext/WarmupHMCLogDensityProblemsADExt.jl
+++ ext/WarmupHMCLogDensityProblemsADExt.jl
@@ -2,8 +2,18 @@ module WarmupHMCLogDensityProblemsADExt
 
 using WarmupHMC, LogDensityProblemsAD
 
+import LogDensityProblemsAD: ADGradientWrapper
+
 WarmupHMC.reparametrize(source::ADGradientWrapper, target::ADGradientWrapper, draw) = begin 
     WarmupHMC.reparametrize(parent(source), parent(target), draws)
 end
+
+WarmupHMC.lja(source::ADGradientWrapper, target::ADGradientWrapper, draw) = begin 
+    WarmupHMC.lja(parent(source), parent(target), draws)
+end
+
+# WarmupHMC.find_reparametrization(source::ADGradientWrapper, draws::AbstractMatrix) = begin
+#     WarmupHMC.reparametrize(source, WarmupHMC.find_reparametrization(parent(source), draws))
+# end
 
 end

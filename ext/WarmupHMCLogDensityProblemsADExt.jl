a6c6e4ad919c19305f1dd3aecc8c510bde8c2ad9
Nikolas Siccha <nikolas.siccha@aalto.fi>
2023-08-22 12:13:25 +0300

    autocommit

--- ext/WarmupHMCLogDensityProblemsADExt.jl
+++ ext/WarmupHMCLogDensityProblemsADExt.jl
@@ -4,12 +4,20 @@ using WarmupHMC, LogDensityProblemsAD
 
 import LogDensityProblemsAD: ADGradientWrapper
 
-WarmupHMC.reparametrize(source::ADGradientWrapper, target::ADGradientWrapper, draws) = begin 
+WarmupHMC.reparametrize(source::ADGradientWrapper, target::ADGradientWrapper, draws::AbstractMatrix) = begin 
     WarmupHMC.reparametrize(parent(source), parent(target), draws)
 end
 
-WarmupHMC.lja(source::ADGradientWrapper, target::ADGradientWrapper, draws) = begin 
+WarmupHMC.reparametrize(source::ADGradientWrapper, target::ADGradientWrapper, draw::AbstractVector) = begin 
+    WarmupHMC.reparametrize(parent(source), parent(target), draw)
+end
+
+WarmupHMC.lja(source::ADGradientWrapper, target::ADGradientWrapper, draws::AbstractMatrix) = begin 
     WarmupHMC.lja(parent(source), parent(target), draws)
+end
+
+WarmupHMC.lja(source::ADGradientWrapper, target::ADGradientWrapper, draw::AbstractVector) = begin 
+    WarmupHMC.lja(parent(source), parent(target), draw)
 end
 
 # WarmupHMC.find_reparametrization(source::ADGradientWrapper, draws::AbstractMatrix) = begin

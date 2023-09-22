142df521372e64372f4320998e04104dc97401ac
Nikolas Siccha <nikolassiccha@gmail.com>
2023-09-22 12:02:50 +0300

    autocommit

--- src/WarmupHMC.jl
+++ src/WarmupHMC.jl
@@ -5,6 +5,7 @@ export regularize, to_x1, to_xc, klp, klps, approximately_whitened, mcmc_with_re
 using DynamicObjects
 using Random, Distributions, LinearAlgebra
 using LogDensityProblems
+using NaNStatistics
 
 regularize(sample_covariance, no_draws, regularization_no_draws=5, regularization_constant=1e-3) = (
   no_draws / ((no_draws + regularization_no_draws) * (no_draws - 1)) * sample_covariance + regularization_constant * (regularization_no_draws / (no_draws + regularization_no_draws)) * I
@@ -194,7 +195,7 @@ struct Ignore <: Real end
 Base.:+(lhs::Ignore, ::Real) = lhs
 Base.:-(lhs::Ignore, ::Real) = lhs
 
-reparametrization_parameters(::Any) = []
+reparametrization_parameters(::Any) = []::Float64
 reparametrize(source::Any, ::Any) = source
 lja_reparametrize(source, target, draws::AbstractMatrix) = begin 
     rv = lja_reparametrize.([source], [target], eachcol(draws))
@@ -210,7 +211,7 @@ reparametrize(source::Any, target::Any, draws::AbstractMatrix) = lja_reparametri
 # )
 reparametrization_loss(source, target, draws::AbstractMatrix) = begin 
     ljas, reparametrized = lja_reparametrize(source, target, draws)
-    mean(ljas) + sum(log.(std(reparametrized, dims=2)))
+    nanmean(ljas) + nansum(log.(nanstd(reparametrized, dims=2)))
 end
 reparametrization_loss_function(source, draws::AbstractMatrix) = begin 
     loss(v) = reparametrization_loss(source, reparametrize(source, v), draws)

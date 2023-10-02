623bea585a5b839110ae763b4a549bbab648eb98
Nikolas Siccha <nikolassiccha@gmail.com>
2023-10-02 18:00:00 +0300

    autocommit

--- ext/WarmupHMCOptimReverseDiffExt.jl
+++ ext/WarmupHMCOptimReverseDiffExt.jl
@@ -19,17 +19,17 @@ WarmupHMC.find_reparametrization(::Val{:ReverseDiff}, source, draws::AbstractMat
     WarmupHMC.reparametrize(source, Optim.minimizer(optimization_result))
 end
 
-WarmupHMC.find_reparametrization(::Val{:Optim}, source, draws::AbstractMatrix; kwargs...) = begin 
+WarmupHMC.find_reparametrization(::Val{:Optim}, source, draws::AbstractMatrix; iterations=5, kwargs...) = begin 
     init_arg = WarmupHMC.reparametrization_parameters(source)
     if length(init_arg) == 1
         loss = WarmupHMC.reparametrization_loss_function(source, draws)
         optimization_result = optimize(
             loss, init_arg, 
-            Optim.Options(iterations=100)
+            Optim.Options(iterations=iterations)
         )
         WarmupHMC.reparametrize(source, Optim.minimizer(optimization_result))
     else
-        WarmupHMC.find_reparametrization(:ReverseDiff, source, draws; kwargs...)
+        WarmupHMC.find_reparametrization(:ReverseDiff, source, draws; iterations=iterations, kwargs...)
     end
 end
 

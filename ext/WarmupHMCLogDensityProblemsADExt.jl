ec3d24adc2ca2ad7bb59801bd410dd6db9ecb056
Nikolas Siccha <nikolassiccha@gmail.com>
2023-10-03 10:01:35 +0300

    autocommit

--- ext/WarmupHMCLogDensityProblemsADExt.jl
+++ ext/WarmupHMCLogDensityProblemsADExt.jl
@@ -4,36 +4,14 @@ using WarmupHMC, LogDensityProblemsAD
 
 import LogDensityProblemsAD: ADGradientWrapper
 
-# WarmupHMC.reparametrize(source::ADGradientWrapper, target::ADGradientWrapper, draws::AbstractMatrix) = begin 
-#     WarmupHMC.reparametrize(parent(source), parent(target), draws)
-# end
-
 WarmupHMC.lja_reparametrize(source::ADGradientWrapper, target::ADGradientWrapper, draw::AbstractVector) = begin 
     WarmupHMC.lja_reparametrize(parent(source), parent(target), draw)
 end
-WarmupHMC.find_reparametrization(source::ADGradientWrapper, draws::AbstractMatrix) = begin
-    WarmupHMC.reparametrize(source, WarmupHMC.find_reparametrization(parent(source), draws))
+WarmupHMC.find_reparametrization(source::ADGradientWrapper, draws::AbstractMatrix; kwargs...) = begin
+    WarmupHMC.reparametrize(source, WarmupHMC.find_reparametrization(parent(source), draws; kwargs...))
 end
 WarmupHMC.reparametrization_parameters(source::ADGradientWrapper) = WarmupHMC.reparametrization_parameters(
     parent(source)
 ) 
-# WarmupHMC.reparametrize(::ADGradientWrapper, target::ContinuousMultivariateDistribution) = begin
-#     ADgradient(:ReverseDiff, target)
-# end
-# WarmupHMC.reparametrize(source::ADGradientWrapper, target::ADGradientWrapper, draw::AbstractVector) = begin 
-#     WarmupHMC.reparametrize(parent(source), parent(target), draw)
-# end
-
-# # WarmupHMC.lja(source::ADGradientWrapper, target::ADGradientWrapper, draws::AbstractMatrix) = begin 
-# #     WarmupHMC.lja(parent(source), parent(target), draws)
-# # end
-
-# WarmupHMC.lja(source::ADGradientWrapper, target::ADGradientWrapper, draw::AbstractVector) = begin 
-#     WarmupHMC.lja(parent(source), parent(target), draw)
-# end
-
-# WarmupHMC.find_reparametrization(source::ADGradientWrapper, draws::AbstractMatrix) = begin
-#     WarmupHMC.reparametrize(source, WarmupHMC.find_reparametrization(parent(source), draws))
-# end
 
 end

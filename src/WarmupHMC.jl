4e221ad537ff7fff81ca4918590de9cf14e1ba63
Nikolas Siccha <nikolassiccha@gmail.com>
2023-10-05 10:55:32 +0300

    autocommit

--- src/WarmupHMC.jl
+++ src/WarmupHMC.jl
@@ -21,7 +21,7 @@ lja_reparametrize(source, target, draws::AbstractMatrix, lja=0.) = begin
     # first.(rv), hcat(last.(rv)...)
 end
 lja_reparametrize(source, target, draws::AbstractVector{<:NamedTuple}, lja=0.) = begin 
-    rv = lja_reparametrize.([source], [target], draws, lja)
+    rv = lja_reparametrize.([source], [target], draws, [lja])
     first.(rv), hcat(last.(rv)...)
 end
 
@@ -61,7 +61,7 @@ end
 reparametrization_loss_function(source, draws::AbstractVector{<:NamedTuple}) = begin 
     loss(v) = reparametrization_loss(source, reparametrize(source, v), draws)
 end
-find_reparametrization(source::UnivariateDistribution, ::AbstractMatrix; kwargs...) = source
+find_reparametrization(source::UnivariateDistribution, ::Any; kwargs...) = source
 find_reparametrization(kind::Symbol, source, draws; kwargs...) = find_reparametrization(Val{kind}(), source, draws; kwargs...)
 # find_reparametrization_kwargs()
 function mcmc_with_reparametrization end

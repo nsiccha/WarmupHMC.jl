4260f6c369a5a705bcf1afa0b9de33aaa078656a
Nikolas Siccha <nikolassiccha@gmail.com>
2023-10-05 23:21:38 +0300

    autocommit

--- ext/WarmupHMCOptimReverseDiffExt.jl
+++ ext/WarmupHMCOptimReverseDiffExt.jl
@@ -11,36 +11,36 @@ WarmupHMC.find_reparametrization(::Val{:ReverseDiff}, source, draws; iterations=
     else 
         (g, arg) -> ReverseDiff.gradient!(g, loss, arg)
     end
-    try
-        optimization_result = optimize(
-            loss, loss_g!, init_arg, method, 
-            Optim.Options(iterations=iterations)
-        )
-        @debug optimization_result
-        WarmupHMC.reparametrize(source, Optim.minimizer(optimization_result))
-    catch e
-        @warn """
-Failed to reparametrize ($iterations, $method): 
-    $source
-    $draws
-    $(WarmupHMC.exception_to_string(e))
-Not reparametrizing...
-        """
-        source
-    end
+    optimization_result = optimize(
+        loss, loss_g!, init_arg, method, 
+        Optim.Options(iterations=iterations)
+    )
+    @debug optimization_result
+    WarmupHMC.reparametrize(source, Optim.minimizer(optimization_result))
 end
 
 WarmupHMC.find_reparametrization(::Val{:Optim}, source, draws; iterations=16, kwargs...) = begin 
     init_arg = WarmupHMC.reparametrization_parameters(source)
-    if length(init_arg) == 1
-        loss = WarmupHMC.reparametrization_loss_function(source, draws)
-        optimization_result = optimize(
-            loss, init_arg, 
-            Optim.Options(iterations=iterations)
-        )
-        WarmupHMC.reparametrize(source, Optim.minimizer(optimization_result))
-    else
-        WarmupHMC.find_reparametrization(:ReverseDiff, source, draws; iterations=iterations, kwargs...)
+    try
+        if length(init_arg) == 1
+            loss = WarmupHMC.reparametrization_loss_function(source, draws)
+            optimization_result = optimize(
+                loss, init_arg, 
+                Optim.Options(iterations=iterations)
+            )
+            WarmupHMC.reparametrize(source, Optim.minimizer(optimization_result))
+        else
+            WarmupHMC.find_reparametrization(:ReverseDiff, source, draws; iterations=iterations, kwargs...)
+        end
+    catch e
+        @warn """
+Failed to reparametrize ($iterations, $kwargs): 
+$source
+$draws
+$(WarmupHMC.exception_to_string(e))
+Not reparametrizing...
+        """
+        source
     end
 end
 

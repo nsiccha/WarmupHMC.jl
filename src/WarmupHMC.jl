497a2484c3e44aff3bcc0ea75c6190fc764e56a1
Nikolas Siccha <nikolassiccha@gmail.com>
2023-10-05 10:18:19 +0300

    autocommit

--- src/WarmupHMC.jl
+++ src/WarmupHMC.jl
@@ -16,7 +16,7 @@ Base.:-(lhs::Ignore, ::Real) = lhs
 reparametrization_parameters(::Any) = Float64[]
 reparametrize(source::Any, ::Any) = source
 lja_reparametrize(source, target, draws::AbstractMatrix, lja=0.) = begin 
-    lja_reparametrize(source, target, lpdf_and_invariants.([source], eachcol(draws), Ignore()), lja)
+    lja_reparametrize(source, target, lpdf_and_invariants(source, draws, Ignore()), lja)
     # rv = lja_reparametrize.([source], [target], eachcol(draws), lja)
     # first.(rv), hcat(last.(rv)...)
 end
@@ -41,6 +41,7 @@ lpdf_and_invariants(source, draw::AbstractVector, lpdf=0.) = begin
     lpdf += sum(logpdf.(source, draw))
     (;lpdf, draw)
 end
+lpdf_and_invariants(source, draws::AbstractMatrix, lpdf=0.) = lpdf_and_invariants.([source], eachcol(draws), [lpdf])
 
 # lja(source::Any, target::Any, draw::AbstractVector) = lja_reparametrize(source, target, draw)[1]
 # lja(source::Any, target::Any, draws::AbstractMatrix) = lja_reparametrize(source, target, draws)[1]
@@ -55,11 +56,13 @@ reparametrization_loss(source, target, draws) = begin
     nanmean(ljas) + nansum(log.(nanstd(reparametrized, dims=2)))
 end
 reparametrization_loss_function(source, draws::AbstractMatrix) = begin 
-    lais = lpdf_and_invariants.([source], eachcol(draws), [Ignore()])
-    loss(v) = reparametrization_loss(source, reparametrize(source, v), lais)
+    reparametrization_loss_function(source, lpdf_and_invariants(source, draws, Ignore()))
+end
+reparametrization_loss_function(source, draws::AbstractVector{<:NamedTuple}) = begin 
+    loss(v) = reparametrization_loss(source, reparametrize(source, v), draws)
 end
 find_reparametrization(source::UnivariateDistribution, ::AbstractMatrix; kwargs...) = source
-find_reparametrization(kind::Symbol, source, draws::AbstractMatrix; kwargs...) = find_reparametrization(Val{kind}(), source, draws; kwargs...)
+find_reparametrization(kind::Symbol, source, draws; kwargs...) = find_reparametrization(Val{kind}(), source, draws; kwargs...)
 # find_reparametrization_kwargs()
 function mcmc_with_reparametrization end
 function mcmc_keep_reparametrization end

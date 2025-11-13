f43ba927fced128a9341f98ae9d56ed7fd7526df
nsiccha <nikolassiccha@gmail.com>
2025-11-13 10:08:29 +0000

    "fix SerializationExt typo"

--- ext/SerializationExt.jl
+++ ext/SerializationExt.jl
@@ -5,7 +5,7 @@ import WarmupHMC
 
 WarmupHMC.restore(path) = isfile(path) ? Serialization.deserialize(path) : nothing
 WarmupHMC.store(path, state) = Serialization.serialize(path, state)
-WarmupHMC.initialize_state(::Nothing, state_path::AbstractString) = WarmupHMC.initialize_state(isfile(state_path) ? .deserialize(state_path) : (;), state_path)
+WarmupHMC.initialize_state(::Nothing, state_path::AbstractString) = WarmupHMC.initialize_state(isfile(state_path) ? Serialization.deserialize(state_path) : (;), state_path)
 WarmupHMC.save_state(state::NamedTuple) = if hasproperty(state, :state_path)
     @info "Writing to $(state.state_path)!" 
     Serialization.serialize(state.state_path, state)
